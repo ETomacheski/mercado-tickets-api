f7b5c150545e6c0b00812e185293304f8e13750c
Eduardo Dias <carloseduardodiasm@gmail.com>
2020-07-02 18:10:43 -0300

    setting up the cors

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@ require('dotenv').config()
 
 const express = require('express')
 const cookieParser = require('cookie-parser')
+const cors = require('cors')
 
 const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors())
 
 const routes = require('./routes')
 app.use(routes)
